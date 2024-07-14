dd6d3c70e4cc4b910dfa5fbc463c337a5f1434d9
LidetGetachew <lidet.getachew@walletbirr.com>
2024-07-14 22:24:54 +0300

    third commit21

--- quiz.js
+++ quiz.js
@@ -2,15 +2,15 @@ function checkAnswer(){
     correctAnswer="4";
 
 let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
-if(userAnswer){
+// if(userAnswer){
     if(userAnswer==correctAnswer){
         document.getElementById('feedback').textContent="correct! Well done";
     }else{
         document.getElementById('feedback').textContent="That is incorrect.Try again!"
     }
 }
-else{
-    document.getElementById('feedback').textContent="Please Select an answer."
-}
-}
-document.getElementById('submit-answer').addEventListener('click',checkAnswer)
+// else{
+//     document.getElementById('feedback').textContent="Please Select an answer."
+// }
+// }
+// document.getElementById('submit-answer').addEventListener('click',checkAnswer)
