322627db32294114a51e4cb8e959caa70483363a
LidetGetachew <lidet.getachew@walletbirr.com>
2024-07-14 20:42:33 +0300

    second commit

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkanswer(){
+    correctAnswer="4";
+
+let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+if(userAnswer){
+    if(userAnswer==correctAnswer){
+        document.getElementById('feedback').textContent="correct! Well done";
+    }else{
+        document.getElementById('feedback').textContent="That is incorrect.Try again!"
+    }
+}
+else{
+    document.getElementById('feedback').textContent="Please Select an answer."
+}
+}
+document.getElementById('submit-answer').addEventListener('click',checkanswer)
